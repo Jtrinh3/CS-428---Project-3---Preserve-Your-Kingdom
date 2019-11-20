6d45e2ecc055b3424c4fd31e773932f453bcefbf
Omar <oalkha2@uic.edu>
2019-11-20 01:28:44 -0600

    added health and tank destruction

---
+++ Assets/Scripts/CastleScript.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleScript : MonoBehaviour
+{
+    public Transform Castle;
+    private int max_hp, curr_hp, gold, damage;
+    private bool destroyed;
+
+    public int get_max_hp()
+    {
+        return max_hp;
+    }
+    public int get_curr_hp()
+    {
+        return curr_hp;
+    }
+    public int get_gold()
+    {
+        return gold;
+    }
+
+    public void gain_gold(int gainz)
+    {
+        gold += gainz;
+    }
+
+    public bool isDestroyed()
+    {
+        if (curr_hp <= 0) destroyed = true;
+        return destroyed;
+    }
+
+    //damages the castle
+    //enemy damage is the modifier (more difficult enemy gives more damage)
+    public void TakeDamage(int enemyDamage)
+    {
+        curr_hp -= damage + enemyDamage;
+        isDestroyed();
+    }
+
+    //routine for damage
+    IEnumerator Damaged(int enemyDamage)
+    {
+        while (true)
+        {
+            TakeDamage(enemyDamage);
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+
+    //starts damage coroutine
+    //returns routine that is started
+    public Coroutine StartDamage(int enemyDamage)
+    {
+        return StartCoroutine(Damaged(enemyDamage));
+    }
+
+
+    //ends damage coroutine
+    //takes routine to end
+    public void EndDamage(Coroutine routine)
+    {
+        StopCoroutine(routine);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Castle = GameObject.Find("Castle").transform;
+        destroyed = false;
+        max_hp = 100;
+        curr_hp = max_hp;
+        gold = 0;
+
+        //when difficulty is implemented damage will change accordingly
+        damage = 1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       gameObject.GetComponentInChildren<TextMesh>().text = curr_hp.ToString();
+    }
+
+
+
+
+
+ /*   private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            InvokeRepeating("takeDamage", 1.0f, 1.0f);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            StartCoroutine("takeDamage");
+            InvokeRepeating("takeDamage", 1.0f, 1.0f);
+        }
+    }*/
+
+}
---
+++ Assets/Scripts/TankScript.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankScript : MonoBehaviour
+{
+    //static variables are shared between game objects
+    private bool isGrabbed = false;
+    private bool wasGrabbed = false;
+    private bool destroyed = false;
+    private bool isCollided = false;
+    public Transform Target; //set target in script component in unity
+    private float curr_speed;
+    private float speed;
+    private Transform turret;
+    private Transform body;
+    private Coroutine damageRoutine;
+
+    public void objectIsGrabbed()
+    {
+        isGrabbed = true;
+        wasGrabbed = true;
+    }
+
+    public void objectIsReleased()
+    {
+        isGrabbed = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5.0f;
+        curr_speed = speed;
+        turret = transform.GetChild(0).GetChild(0).GetChild(0).transform;
+        body = transform.GetChild(0).GetChild(0).transform;
+    }
+
+    private void moveTowardTarget()
+    {
+        // Move our position a step closer to the target.
+        float step = curr_speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(transform.position, Target.position) < 3f || destroyed)
+        {
+            curr_speed = 0.0f;
+        }
+        else speed = curr_speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        moveTowardTarget();
+
+        checkVelocity();
+
+        turret.LookAt(Target);
+        turret.Rotate(new Vector3(-90, 90, 0));
+        //turret facement to target
+        if (isGrabbed == false)
+        {
+            body.LookAt(Target.position - transform.position);
+            body.Rotate(new Vector3(-90, 90, 0));
+        }
+    }
+
+    public IEnumerator breakEnemy()
+    {
+        transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material = Target.GetComponent<Renderer>().material; ;
+        transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>().material = Target.GetComponent<Renderer>().material; ;
+
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
+
+    void checkVelocity()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 velocity = rb.velocity;
+        float avgVelocity = (velocity.x + velocity.y + velocity.z)/3;
+        if (avgVelocity > 2f && wasGrabbed)
+        {
+            destroyed = true;
+            StartCoroutine(breakEnemy());
+        }
+    }
+
+
+    //if colliding with castle begin damage routine
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Castle")
+        {
+            GameObject c = GameObject.Find("Castle");
+            damageRoutine = c.GetComponent<CastleScript>().StartDamage(0); //change inner number for additional damage
+            isCollided = true;
+        }
+    }
+
+    //at end of collission end damage
+    private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Castle")
+        {
+            GameObject c = GameObject.Find("Castle");
+            c.GetComponent<CastleScript>().EndDamage(damageRoutine);
+            isCollided = false;
+        }
+    }
+
+}
